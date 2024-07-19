e776424e6827b8e876a1ee29c171cc0e415784e8
immortal-forest <ali.salman.sa.01@gmail.com>
2024-07-19 22:48:19 +0545

    fix: fix workflows and scripts

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,8 +2,8 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore ../master/repo/ .
-git config --global user.email "aniyomi-bot@aniyomi.org"
-git config --global user.name "aniyomi-bot[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
@@ -11,7 +11,7 @@ if [ -n "$(git status --porcelain)" ]; then
     git push
 
     # Purge cached index on jsDelivr
-    curl https://purge.jsdelivr.net/gh/aniyomiorg/aniyomi-extensions@repo/index.min.json
+    curl https://purge.jsdelivr.net/gh/immortal-forest/aniyomi-extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
